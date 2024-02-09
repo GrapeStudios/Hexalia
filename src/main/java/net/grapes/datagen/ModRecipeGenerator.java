974885de2f508566837437a169127d64d5827775
SirColor <macaraballo12@gmail.com>
2024-02-09 01:30:12 -0400

    Added shapeless recipes for Mortar and Pestle.

--- src/main/java/net/grapes/datagen/ModRecipeGenerator.java
+++ src/main/java/net/grapes/datagen/ModRecipeGenerator.java
@@ -5,6 +5,8 @@ import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.grapes.hexalia.block.ModBlocks;
 import net.grapes.hexalia.item.ModItems;
 import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
+import net.minecraft.recipe.ShapelessRecipe;
 import net.minecraft.recipe.book.RecipeCategory;
 
 import java.util.function.Consumer;
@@ -22,5 +24,25 @@ public class ModRecipeGenerator extends FabricRecipeProvider {
 
         offerReversibleCompactingRecipes(exporter, RecipeCategory.MISC, ModItems.SALT,
                 RecipeCategory.MISC, ModBlocks.SALT_BLOCK);
+
+        // Shapeless Recipe for Mortar & Pestle
+
+        ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.SPIRIT_BLOOM_DUST, 1)
+                .input(ModItems.MORTAR_AND_PESTLE)
+                .input(ModItems.SPIRIT_BLOOM_PETAL)
+                .criterion(hasItem(ModItems.MORTAR_AND_PESTLE), conditionsFromItem(ModItems.MORTAR_AND_PESTLE))
+                .offerTo(exporter);
+
+        ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.SIREN_KELP_PASTE, 1)
+                .input(ModItems.MORTAR_AND_PESTLE)
+                .input(ModItems.SIREN_KELP)
+                .criterion(hasItem(ModItems.MORTAR_AND_PESTLE), conditionsFromItem(ModItems.MORTAR_AND_PESTLE))
+                .offerTo(exporter);
+
+        ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.DREAMSHROOM_DUST, 1)
+                .input(ModItems.MORTAR_AND_PESTLE)
+                .input(ModBlocks.DREAMSHROOM)
+                .criterion(hasItem(ModItems.MORTAR_AND_PESTLE), conditionsFromItem(ModItems.MORTAR_AND_PESTLE))
+                .offerTo(exporter);
     }
 }
