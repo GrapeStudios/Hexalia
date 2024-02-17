1eb68dbdc857afc697d2856805b7d653d3d0f6ea
SirColor <sircoloryt@gmail.com>
2024-02-16 23:28:27 -0400

    Fixed the onUse method to only return max. 2 blocks after stage 3.

--- src/main/java/net/grapes/hexalia/block/custom/SunfireTomatoCropBlock.java
+++ src/main/java/net/grapes/hexalia/block/custom/SunfireTomatoCropBlock.java
@@ -4,9 +4,20 @@ import net.grapes.hexalia.item.ModItems;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.CropBlock;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.IntProperty;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
 
 public class SunfireTomatoCropBlock extends CropBlock {
     public static final int MAX_AGE = 3;
@@ -34,5 +45,23 @@ public class SunfireTomatoCropBlock extends CropBlock {
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
         builder.add(AGE);
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        int i = state.get(AGE);
+        if (i != 3) {
+            return super.onUse(state, world, pos, player, hand, hit);
+        }
+        if (player.getStackInHand(hand).isOf(Items.BONE_MEAL)) {
+            return ActionResult.PASS;
+        }
+        int j = 1 + world.random.nextInt(1);
+        SunfireTomatoCropBlock.dropStack(world, pos, new ItemStack(ModItems.SUNFIRE_TOMATO, j + 1));
+        world.playSound(null, pos, SoundEvents.BLOCK_SWEET_BERRY_BUSH_PICK_BERRIES, SoundCategory.BLOCKS, 1.0f, 0.8f + world.random.nextFloat() * 0.4f);
+        BlockState blockState = (BlockState)state.with(AGE, 1);
+        world.setBlockState(pos, blockState, Block.NOTIFY_LISTENERS);
+        world.emitGameEvent(GameEvent.BLOCK_CHANGE, pos, GameEvent.Emitter.of(player, blockState));
+        return ActionResult.success(world.isClient);
     }
 }
