a29e5aca125e273e018384595144517067ad94ca
SirColor <sircoloryt@gmail.com>
2024-02-16 23:28:57 -0400

    Fixed spacing.

--- src/main/java/net/grapes/hexalia/block/custom/ChillberryBushBlock.java
+++ src/main/java/net/grapes/hexalia/block/custom/ChillberryBushBlock.java
@@ -45,21 +45,19 @@ public class ChillberryBushBlock extends SweetBerryBushBlock {
 
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-        boolean bl;
         int i = state.get(AGE);
-        boolean bl2 = bl = i == 3;
-        if (!bl && player.getStackInHand(hand).isOf(Items.BONE_MEAL)) {
+        if (i != 3) {
+            return super.onUse(state, world, pos, player, hand, hit);
+        }
+        if (player.getStackInHand(hand).isOf(Items.BONE_MEAL)) {
             return ActionResult.PASS;
         }
-        if (i > 1) {
-            int j = 1 + world.random.nextInt(2);
-            ChillberryBushBlock.dropStack(world, pos, new ItemStack(ModItems.CHILLBERRY, j + (bl ? 1 : 0)));
-            world.playSound(null, pos, SoundEvents.BLOCK_SWEET_BERRY_BUSH_PICK_BERRIES, SoundCategory.BLOCKS, 1.0f, 0.8f + world.random.nextFloat() * 0.4f);
-            BlockState blockState = (BlockState)state.with(AGE, 1);
-            world.setBlockState(pos, blockState, Block.NOTIFY_LISTENERS);
-            world.emitGameEvent(GameEvent.BLOCK_CHANGE, pos, GameEvent.Emitter.of(player, blockState));
-            return ActionResult.success(world.isClient);
-        }
-        return super.onUse(state, world, pos, player, hand, hit);
+        int j = 1 + world.random.nextInt(2);
+        ChillberryBushBlock.dropStack(world, pos, new ItemStack(ModItems.CHILLBERRY, j + 1));
+        world.playSound(null, pos, SoundEvents.BLOCK_SWEET_BERRY_BUSH_PICK_BERRIES, SoundCategory.BLOCKS, 1.0f, 0.8f + world.random.nextFloat() * 0.4f);
+        BlockState blockState = (BlockState)state.with(AGE, 1);
+        world.setBlockState(pos, blockState, Block.NOTIFY_LISTENERS);
+        world.emitGameEvent(GameEvent.BLOCK_CHANGE, pos, GameEvent.Emitter.of(player, blockState));
+        return ActionResult.success(world.isClient);
     }
 }
