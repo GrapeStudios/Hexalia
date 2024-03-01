6906716f3905d36606fc1da6971bb6319e647f39
SirColor <sircoloryt@gmail.com>
2024-03-01 02:14:08 -0400

    Added spore particles to Dreamshroom

--- src/main/java/net/grapes/hexalia/block/custom/DreamshroomBlock.java
+++ src/main/java/net/grapes/hexalia/block/custom/DreamshroomBlock.java
@@ -3,17 +3,11 @@ package net.grapes.hexalia.block.custom;
 import net.grapes.hexalia.particle.ModParticles;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.MushroomPlantBlock;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
-import net.minecraft.world.Difficulty;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.TreeConfiguredFeatures;
-
-import java.util.concurrent.ThreadLocalRandom;
 
 public class DreamshroomBlock extends MushroomPlantBlock {
     public DreamshroomBlock(Settings settings) {
@@ -27,7 +21,7 @@ public class DreamshroomBlock extends MushroomPlantBlock {
 
     @Override
     public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
-        createSporeParticles(world, pos, 5);
+        createSporeParticles(world, pos, random,5);
     }
 
     @Override
@@ -35,19 +29,22 @@ public class DreamshroomBlock extends MushroomPlantBlock {
         return false;
     }
 
-    public static void createSporeParticles(World world, BlockPos pos, int particleFrequency) {
+    public static void createSporeParticles(World world, BlockPos pos, Random random, int particleFrequency) {
         final double maxHorizontalOffset = 0.1;
-        ThreadLocalRandom random = ThreadLocalRandom.current();
-        double yStart = pos.getY() + 0.3;
-        for (double y = yStart; y > pos.getY(); y -= 0.05) {
+        double centerX = pos.getX() + 0.5;
+        double centerZ = pos.getZ() + 0.5;
+
+        for (double y = pos.getY() + 0.3; y > pos.getY(); y -= 0.1) {
             if (random.nextInt(particleFrequency) == 0) {
-                double x = pos.getX() + 0.5;
-                double z = pos.getZ() + 0.5;
-                z += random.nextDouble(-maxHorizontalOffset, maxHorizontalOffset);
-                x += random.nextDouble(-maxHorizontalOffset, maxHorizontalOffset);
-                world.addParticle(ModParticles.SPORE_PARTICLE, x, y, z, 0.001D, -0.001D, 0.001D); // Adjusting the Y velocity to be negative
+                double x = centerX + random.nextDouble() * 2 * maxHorizontalOffset - maxHorizontalOffset;
+                double z = centerZ + random.nextDouble() * 2 * maxHorizontalOffset - maxHorizontalOffset;
+                double motionX = random.nextGaussian() * 0.02;
+                double motionY = random.nextGaussian() * 0.02;
+                double motionZ = random.nextGaussian() * 0.02;
+                world.addParticle(ModParticles.SPORE_PARTICLE, x, y, z, motionX, motionY, motionZ);
             }
         }
     }
+
 
 }
