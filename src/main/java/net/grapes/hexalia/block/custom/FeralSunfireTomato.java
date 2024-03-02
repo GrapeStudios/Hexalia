936fec11c6bc016becb858d0877decac15671895
SirColor <sircoloryt@gmail.com>
2024-03-02 08:51:11 -0400

    Added VoxelShape

--- src/main/java/net/grapes/hexalia/block/custom/FeralSunfireTomato.java
+++ src/main/java/net/grapes/hexalia/block/custom/FeralSunfireTomato.java
@@ -1,27 +1,38 @@
 package net.grapes.hexalia.block.custom;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.PlantBlock;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.Difficulty;
 import net.minecraft.world.World;
 
 import java.util.concurrent.ThreadLocalRandom;
 
 public class FeralSunfireTomato extends PlantBlock {
+    protected static final VoxelShape SHAPE = Block.createCuboidShape(5.0, 0.0, 5.0, 11.0, 10.0, 11.0);
     public FeralSunfireTomato(Settings settings) {
         super(settings);
     }
-
 
     @Override
     public void onBreak(World world, BlockPos pos, BlockState state, PlayerEntity player) {
         createFireParticles(world, pos);
         super.onBreak(world, pos, state, player);
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        Vec3d vec3d = state.getModelOffset(world, pos);
+        return SHAPE.offset(vec3d.x, vec3d.y, vec3d.z);
     }
 
     @Override
