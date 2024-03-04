08cd3be1b7d515367ee262f39a726e346a7a0a08
SirColor <sircoloryt@gmail.com>
2024-03-04 10:31:25 -0400

    Siren Kelp

--- src/main/java/net/grapes/hexalia/world/gen/feature/HexaliaFeatures.java
+++ src/main/java/net/grapes/hexalia/world/gen/feature/HexaliaFeatures.java
@@ -2,10 +2,10 @@ package net.grapes.hexalia.world.gen.feature;
 
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.world.gen.ProbabilityConfig;
+import net.minecraft.world.gen.CountConfig;
 
 public class HexaliaFeatures {
-    public static final SirenKelpFeature SIREN_KELP = new SirenKelpFeature(ProbabilityConfig.CODEC);
+    public static final SirenKelpFeature SIREN_KELP = new SirenKelpFeature(CountConfig.CODEC);
 
     public static void registerFeature() {
         Registry.register(Registries.FEATURE, ("siren_kelp"), SIREN_KELP);
--- src/main/java/net/grapes/hexalia/world/gen/feature/SirenKelpFeature.java
+++ src/main/java/net/grapes/hexalia/world/gen/feature/SirenKelpFeature.java
@@ -1,54 +1,37 @@
 package net.grapes.hexalia.world.gen.feature;
 
 import com.mojang.serialization.Codec;
-import net.grapes.hexalia.block.ModBlocks;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.TallSeagrassBlock;
-import net.minecraft.block.enums.DoubleBlockHalf;
+import net.minecraft.block.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.Heightmap;
 import net.minecraft.world.StructureWorldAccess;
-import net.minecraft.world.gen.ProbabilityConfig;
+import net.minecraft.world.gen.CountConfig;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.util.FeatureContext;
 
-public class SirenKelpFeature extends Feature<ProbabilityConfig> {
-    public SirenKelpFeature(Codec<ProbabilityConfig> configCodec) {
-        super(configCodec);
+public class SirenKelpFeature extends Feature<CountConfig> {
+    public SirenKelpFeature(Codec<CountConfig> codec) {
+        super(codec);
     }
 
     @Override
-    public boolean generate(FeatureContext<ProbabilityConfig> context) {
-        boolean bl = false;
+    public boolean generate(FeatureContext<CountConfig> context) {
+        int i = 0;
         Random random = context.getRandom();
         StructureWorldAccess structureWorldAccess = context.getWorld();
         BlockPos blockPos = context.getOrigin();
-        ProbabilityConfig probabilityConfig = context.getConfig();
-        int i = random.nextInt(8) - random.nextInt(8);
-        int j = random.nextInt(8) - random.nextInt(8);
-        int k = structureWorldAccess.getTopY(Heightmap.Type.OCEAN_FLOOR, blockPos.getX() + i, blockPos.getZ() + j);
-        BlockPos blockPos2 = new BlockPos(blockPos.getX() + i, k, blockPos.getZ() + j);
-        if (structureWorldAccess.getBlockState(blockPos2).isOf(Blocks.WATER)) {
-            BlockState blockState;
-            boolean bl2 = random.nextDouble() < (double)probabilityConfig.probability;
-                BlockState blockState2 = blockState = ModBlocks.SIREN_KELP.getDefaultState();
-            if (blockState.canPlaceAt(structureWorldAccess, blockPos2)) {
-                if (bl2) {
-                    BlockState blockState22 = (BlockState)blockState.with(TallSeagrassBlock.HALF, DoubleBlockHalf.UPPER);
-                    BlockPos blockPos3 = blockPos2.up();
-                    if (structureWorldAccess.getBlockState(blockPos3).isOf(Blocks.WATER)) {
-                        structureWorldAccess.setBlockState(blockPos2, blockState, Block.NOTIFY_LISTENERS);
-                        structureWorldAccess.setBlockState(blockPos3, blockState22, Block.NOTIFY_LISTENERS);
-                    }
-                } else {
-                    structureWorldAccess.setBlockState(blockPos2, blockState, Block.NOTIFY_LISTENERS);
-                }
-                bl = true;
-            }
+        int j = context.getConfig().getCount().get(random);
+        for (int k = 0; k < j; ++k) {
+            int l = random.nextInt(8) - random.nextInt(8);
+            int m = random.nextInt(8) - random.nextInt(8);
+            int n = structureWorldAccess.getTopY(Heightmap.Type.OCEAN_FLOOR, blockPos.getX() + l, blockPos.getZ() + m);
+            BlockPos blockPos2 = new BlockPos(blockPos.getX() + l, n, blockPos.getZ() + m);
+            BlockState blockState = (BlockState)Blocks.SEA_PICKLE.getDefaultState().with(SeaPickleBlock.PICKLES, random.nextInt(4) + 1);
+            if (!structureWorldAccess.getBlockState(blockPos2).isOf(Blocks.WATER) || !blockState.canPlaceAt(structureWorldAccess, blockPos2)) continue;
+            structureWorldAccess.setBlockState(blockPos2, blockState, Block.NOTIFY_LISTENERS);
+            ++i;
         }
-        return bl;
+        return i > 0;
     }
 }
