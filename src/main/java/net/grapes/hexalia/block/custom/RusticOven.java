73585df828d91f20ee19d2155e6884b3d1011f3c
SirColor <sircoloryt@gmail.com>
2024-04-10 14:00:38 -0400

    Add randomDisplayTick on RusticOven

--- src/main/java/net/grapes/hexalia/block/custom/RusticOven.java
+++ src/main/java/net/grapes/hexalia/block/custom/RusticOven.java
@@ -5,19 +5,24 @@ import net.minecraft.block.BlockState;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.fluid.FluidState;
-import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 public class RusticOven extends Block {
 
     public static final DirectionProperty FACING = Properties.HORIZONTAL_FACING;
+    public static final BooleanProperty LIT = Properties.LIT;
 
     public RusticOven(Settings settings) {
         super(settings);
@@ -28,7 +33,6 @@ public class RusticOven extends Block {
                 entity instanceof LivingEntity && !EnchantmentHelper.hasFrostWalker((LivingEntity)entity)) {
             entity.damage(world.getDamageSources().hotFloor(), 1.0F);
         }
-
         super.onSteppedOn(world, pos, state, entity);
     }
 
@@ -36,6 +40,25 @@ public class RusticOven extends Block {
     @Override
     public BlockState getPlacementState(ItemPlacementContext ctx) {
         return this.getDefaultState().with(FACING, ctx.getHorizontalPlayerFacing().getOpposite());
+    }
+
+    public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
+            double d = (double)pos.getX() + 0.5;
+            double e = (double)pos.getY();
+            double f = (double)pos.getZ() + 0.5;
+            if (random.nextDouble() < 0.1) {
+                world.playSound(d, e, f, SoundEvents.BLOCK_FURNACE_FIRE_CRACKLE, SoundCategory.BLOCKS, 1.0F, 1.0F, false);
+
+            Direction direction = (Direction)state.get(FACING);
+            Direction.Axis axis = direction.getAxis();
+            double g = 0.52;
+            double h = random.nextDouble() * 0.6 - 0.3;
+            double i = axis == Direction.Axis.X ? (double)direction.getOffsetX() * 0.52 : h;
+            double j = random.nextDouble() * 6.0 / 16.0;
+            double k = axis == Direction.Axis.Z ? (double)direction.getOffsetZ() * 0.52 : h;
+            world.addParticle(ParticleTypes.SMOKE, d + i, e + j, f + k, 0.0, 0.0, 0.0);
+            world.addParticle(ParticleTypes.FLAME, d + i, e + j, f + k, 0.0, 0.0, 0.0);
+        }
     }
 
     @Override
