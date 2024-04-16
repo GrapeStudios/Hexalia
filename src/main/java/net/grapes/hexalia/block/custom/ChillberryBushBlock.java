657108b4b525c72889e0fb7838fadade0216c85a
SirColor <sircoloryt@gmail.com>
2024-04-16 09:44:10 -0400

    Fix ChillberryBushBlock bug

--- src/main/java/net/grapes/hexalia/block/custom/ChillberryBushBlock.java
+++ src/main/java/net/grapes/hexalia/block/custom/ChillberryBushBlock.java
@@ -3,25 +3,33 @@ package net.grapes.hexalia.block.custom;
 import net.grapes.hexalia.item.ModItems;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.SweetBerryBushBlock;
+import net.minecraft.block.Fertilizable;
+import net.minecraft.block.PlantBlock;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.IntProperty;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldView;
 import net.minecraft.world.event.GameEvent;
 
-public class ChillberryBushBlock extends SweetBerryBushBlock {
+public class ChillberryBushBlock extends PlantBlock implements Fertilizable {
+    public static final int MAX_AGE = 3;
+    public static final IntProperty AGE = IntProperty.of("age", 0, 3);
     public ChillberryBushBlock(Settings settings) {
         super(settings);
     }
@@ -46,18 +54,40 @@ public class ChillberryBushBlock extends SweetBerryBushBlock {
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
         int i = state.get(AGE);
-        if (i != 3) {
+        boolean bl = i == 3;
+        if (!bl && player.getStackInHand(hand).isOf(Items.BONE_MEAL)) {
+            return ActionResult.PASS;
+        } else if (i > 1) {
+            int j = 1 + world.random.nextInt(2);
+            dropStack(world, pos, new ItemStack(ModItems.CHILLBERRIES, j + (bl ? 1 : 0)));
+            world.playSound(null, pos, SoundEvents.BLOCK_SWEET_BERRY_BUSH_PICK_BERRIES, SoundCategory.BLOCKS, 1.0F, 0.8F + world.random.nextFloat() * 0.4F);
+            BlockState blockState = state.with(AGE, 1);
+            world.setBlockState(pos, blockState, 2);
+            world.emitGameEvent(GameEvent.BLOCK_CHANGE, pos, GameEvent.Emitter.of(player, blockState));
+            return ActionResult.success(world.isClient);
+        } else {
             return super.onUse(state, world, pos, player, hand, hit);
         }
-        if (player.getStackInHand(hand).isOf(Items.BONE_MEAL)) {
-            return ActionResult.PASS;
-        }
-        int j = 1 + world.random.nextInt(2);
-        ChillberryBushBlock.dropStack(world, pos, new ItemStack(ModItems.CHILLBERRIES, j + 1));
-        world.playSound(null, pos, SoundEvents.BLOCK_SWEET_BERRY_BUSH_PICK_BERRIES, SoundCategory.BLOCKS, 1.0f, 0.8f + world.random.nextFloat() * 0.4f);
-        BlockState blockState = state.with(AGE, 1);
-        world.setBlockState(pos, blockState, Block.NOTIFY_LISTENERS);
-        world.emitGameEvent(GameEvent.BLOCK_CHANGE, pos, GameEvent.Emitter.of(player, blockState));
-        return ActionResult.success(world.isClient);
+    }
+
+    @Override
+    public boolean isFertilizable(WorldView world, BlockPos pos, BlockState state, boolean isClient) {
+        return state.get(AGE) < 3;
+    }
+
+    @Override
+    public boolean canGrow(World world, Random random, BlockPos pos, BlockState state) {
+        return true;
+    }
+
+    @Override
+    public void grow(ServerWorld world, Random random, BlockPos pos, BlockState state) {
+        int i = Math.min(3, state.get(AGE) + 1);
+        world.setBlockState(pos, state.with(AGE, i), 2);
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(AGE);
     }
 }
