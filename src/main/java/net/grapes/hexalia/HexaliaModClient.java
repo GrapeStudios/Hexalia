bdede6067c36f12a7867adf2096e907109874df3
SirColor <macaraballo12@gmail.com>
2024-06-19 17:52:12 -0400

    Add Hanging Property to Salt Lamp

--- src/main/java/net/grapes/hexalia/HexaliaModClient.java
+++ src/main/java/net/grapes/hexalia/HexaliaModClient.java
@@ -1,5 +1,6 @@
 package net.grapes.hexalia;
 
+import dev.architectury.platform.Mod;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
@@ -37,7 +38,8 @@ public class HexaliaModClient implements ClientModInitializer {
                 ModBlocks.DREAMCATCHER,
                 ModBlocks.POTTED_HENSBANE,
                 ModBlocks.HENSBANE,
-                ModBlocks.CANDLE_SKULL);
+                ModBlocks.CANDLE_SKULL,
+                ModBlocks.SALT_LAMP);
 
         BlockEntityRendererFactories.register(ModBlockEntities.RITUAL_TABLE_BE, RitualTableBlockEntityRenderer::new);
         BlockEntityRendererFactories.register(ModBlockEntities.SALT_BE, SaltBlockEntityRenderer::new);
--- src/main/java/net/grapes/hexalia/block/custom/SaltLampBlock.java
+++ src/main/java/net/grapes/hexalia/block/custom/SaltLampBlock.java
@@ -1,71 +1,75 @@
 package net.grapes.hexalia.block.custom;
 
-import net.grapes.hexalia.particle.ModParticles;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.ShapeContext;
+import net.minecraft.block.*;
+import net.minecraft.entity.ai.pathing.NavigationType;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemPlacementContext;
-import net.minecraft.particle.ParticleTypes;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.random.Random;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldView;
+import net.minecraft.world.WorldAccess;
 import org.jetbrains.annotations.Nullable;
 
-public class SaltLampBlock extends Block {
+public class SaltLampBlock extends LanternBlock {
     public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
+    public static final BooleanProperty HANGING = Properties.HANGING;
 
-    private static final double MIN_X = 0.25;
-    private static final double MAX_X = 0.75;
-    private static final double MIN_Y = 0.0;
-    private static final double MID_Y = 0.125;
-    private static final double MAX_Y = 0.6875;
-
-    public static final VoxelShape SHAPE = VoxelShapes.union(
-            VoxelShapes.cuboid(MIN_X, MIN_Y, MIN_X, MAX_X, MID_Y, MAX_X),
-            VoxelShapes.cuboid(MIN_X, MID_Y, MIN_X, MAX_X, 0.625, MAX_X),
-            VoxelShapes.cuboid(MIN_X, 0.625, MIN_X, MAX_X, MAX_Y, MAX_X));
+    public static final VoxelShape STANDING_SHAPE = VoxelShapes.union(
+            VoxelShapes.cuboid(0.25, 0, 0.25, 0.75, 0.6875, 0.75));
+    public static final VoxelShape HANGING_SHAPE = VoxelShapes.union(
+            VoxelShapes.cuboid(0.25, 0, 0.25, 0.75, 0.6875, 0.75),
+            VoxelShapes.cuboid(0.4375, 0.625, 0.4375, 0.5625, 1.0625, 0.5625));
 
     public SaltLampBlock(Settings settings) {
         super(settings);
-        setDefaultState(getStateManager().getDefaultState().with(WATERLOGGED, false));
+        this.setDefaultState(this.stateManager.getDefaultState().with(HANGING, false).with(WATERLOGGED, false));
     }
 
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-        return SHAPE;
+        return state.get(HANGING) ? HANGING_SHAPE : STANDING_SHAPE;
     }
 
     @Override
-    public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
-        return Block.sideCoversSmallSquare(world, pos.down(), Direction.UP);
-    }
-
     @Nullable
-    @Override
     public BlockState getPlacementState(ItemPlacementContext ctx) {
-        BlockPos blockPos = ctx.getBlockPos();
-        World world = ctx.getWorld();
-        FluidState fluidState = world.getFluidState(blockPos);
-        return getDefaultState().with(WATERLOGGED, fluidState.getFluid() == Fluids.WATER);
+        FluidState fluidState = ctx.getWorld().getFluidState(ctx.getBlockPos());
+        for (Direction direction : ctx.getPlacementDirections()) {
+            BlockState blockState;
+            if (direction.getAxis() != Direction.Axis.Y || !(blockState = this.getDefaultState().with(HANGING, direction == Direction.UP)).canPlaceAt(ctx.getWorld(), ctx.getBlockPos())) continue;
+            return blockState.with(WATERLOGGED, fluidState.getFluid() == Fluids.WATER);
+        }
+        return null;
     }
 
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(WATERLOGGED);
+        builder.add(HANGING, WATERLOGGED);
+    }
+
+    @Override
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+        if (state.get(WATERLOGGED)) {
+            world.scheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+        }
+        if (LanternBlock.attachedDirection(state).getOpposite() == direction && !state.canPlaceAt(world, pos)) {
+            return Blocks.AIR.getDefaultState();
+        }
+        return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
     }
 
     @Override
     public FluidState getFluidState(BlockState state) {
-        return state.get(WATERLOGGED) ? Fluids.WATER.getStill(false) : super.getFluidState(state);
+        if (state.get(WATERLOGGED)) {
+            return Fluids.WATER.getStill(false);
+        }
+        return super.getFluidState(state);
     }
 }
