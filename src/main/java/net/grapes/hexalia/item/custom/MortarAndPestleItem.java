1e8afe5ba18e48eb7e052edcc61cd63bcfa08d3a
SirColor <macaraballo12@gmail.com>
2024-07-01 22:14:37 -0400

    Update MortarAndPestle

--- src/main/java/net/grapes/hexalia/item/custom/MortarAndPestleItem.java
+++ src/main/java/net/grapes/hexalia/item/custom/MortarAndPestleItem.java
@@ -5,11 +5,22 @@ import net.minecraft.item.ItemStack;
 
 public class MortarAndPestleItem extends Item {
     public MortarAndPestleItem(Settings settings) {
-        super(settings);
+        super(settings.maxDamage(64));
+    }
+
+    @Override
+    public boolean hasRecipeRemainder() {
+        return true;
     }
 
     @Override
     public ItemStack getRecipeRemainder(ItemStack stack) {
-        return stack.copy();
+        ItemStack result = stack.copy();
+        result.setDamage(result.getDamage() + 1);
+
+        if (result.getDamage() >= result.getMaxDamage()) {
+            return ItemStack.EMPTY;
+        }
+        return result;
     }
 }
