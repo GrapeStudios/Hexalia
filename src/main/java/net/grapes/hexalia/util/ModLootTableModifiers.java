dd053ebf31696a21ff812de596fbde5ed5e1fa34
SirColor <macaraballo12@gmail.com>
2024-07-06 16:30:08 -0400

    Updated to LootTableEvents as LootTableLoadingCallback is deprecated

--- src/main/java/net/grapes/hexalia/util/ModLootTableModifiers.java
+++ src/main/java/net/grapes/hexalia/util/ModLootTableModifiers.java
@@ -1,5 +1,6 @@
 package net.grapes.hexalia.util;
 
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.grapes.hexalia.item.ModItems;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
@@ -8,18 +9,18 @@ import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
 import net.minecraft.util.Identifier;
-import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
 
 public class ModLootTableModifiers {
+
     private static final Identifier JUNGLE_TEMPLE_CHEST_ID
             = new Identifier("minecraft", "chests/jungle_temple");
 
     public static void modifyLootTables(){
-        LootTableLoadingCallback.EVENT.register((resourceManager, lootManager, id, table, setter) -> {
-            if (id.equals(JUNGLE_TEMPLE_CHEST_ID)) {
+        LootTableEvents.MODIFY.register((resourceManager, lootManager, id, table, source) -> {
+            if (JUNGLE_TEMPLE_CHEST_ID.equals(id)) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(2))
-                        .conditionally(RandomChanceLootCondition.builder(0.50f))
+                        .conditionally(RandomChanceLootCondition.builder(0.5f))
                         .with(ItemEntry.builder(ModItems.ANCIENT_SEED))
                         .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)));
                 table.pool(poolBuilder);
