2379bc49b5baae9b3114f30dd9d64a0b33096f81
SirColor <macaraballo12@gmail.com>
2024-07-28 09:23:43 -0400

    Remove Unnecessary Imports

--- src/main/java/net/grapes/hexalia/world/gen/ModEntitySpawns.java
+++ src/main/java/net/grapes/hexalia/world/gen/ModEntitySpawns.java
@@ -1,6 +1,5 @@
 package net.grapes.hexalia.world.gen;
 
-import net.fabricmc.fabric.api.biome.v1.BiomeModification;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.grapes.hexalia.entity.ModEntities;
