75feabb096180f6338de78d00227de909971c337
SirColor <macaraballo12@gmail.com>
2024-08-21 22:36:49 -0400

    Add AttractedToLightGoal

---
+++ src/main/java/net/grapes/hexalia/entity/ai/AttractedToLightGoal.java
@@ -0,0 +1,64 @@
+package net.grapes.hexalia.entity.ai;
+
+import net.grapes.hexalia.util.ModTags;
+import net.minecraft.block.Block;
+import net.minecraft.entity.ai.goal.Goal;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import java.util.EnumSet;
+
+public class AttractedToLightGoal extends Goal {
+    private final MobEntity entity;
+    private final double speed;
+    private BlockPos targetPos;
+
+    public AttractedToLightGoal(MobEntity entity, double speed) {
+        this.entity = entity;
+        this.speed = speed;
+        this.setControls(EnumSet.of(Control.MOVE));
+    }
+
+    @Override
+    public boolean canStart() {
+        return this.findAttractingLightSource();
+    }
+
+    @Override
+    public void start() {
+        if (this.targetPos != null) {
+            this.entity.getNavigation().startMovingTo(targetPos.getX(), targetPos.getY(), targetPos.getZ(), this.speed);
+        }
+    }
+
+    @Override
+    public boolean shouldContinue() {
+        return this.targetPos != null && !this.entity.getNavigation().isIdle();
+    }
+
+    @Override
+    public void stop() {
+        this.targetPos = null;
+    }
+
+    private boolean findAttractingLightSource() {
+        World world = this.entity.getWorld();
+        BlockPos entityPos = this.entity.getBlockPos();
+        BlockPos.Mutable mutablePos = new BlockPos.Mutable();
+
+        for (int x = -10; x <= 10; x++) {
+            for (int y = -5; y <= 5; y++) {
+                for (int z = -10; z <= 10; z++) {
+                    mutablePos.set(entityPos.getX() + x, entityPos.getY() + y, entityPos.getZ() + z);
+                    Block block = world.getBlockState(mutablePos).getBlock();
+                    if (block.getDefaultState().isIn(ModTags.Blocks.ATTRACTS_MOTH)) {
+                        this.targetPos = mutablePos.toImmutable();
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
