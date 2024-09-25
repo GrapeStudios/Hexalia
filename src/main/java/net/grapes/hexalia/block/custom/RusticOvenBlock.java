0f37655c41419e35888e4e389e8f32f370b186b3
SirColor <macaraballo12@gmail.com>
2024-09-25 10:13:52 -0400

    Fix Blockstate Rotation Issue

--- src/main/java/net/grapes/hexalia/block/custom/RusticOvenBlock.java
+++ src/main/java/net/grapes/hexalia/block/custom/RusticOvenBlock.java
@@ -12,6 +12,8 @@ import net.minecraft.sound.SoundEvents;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.state.property.Properties;
+import net.minecraft.util.BlockMirror;
+import net.minecraft.util.BlockRotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
@@ -63,5 +65,15 @@ public class RusticOvenBlock extends Block {
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
         super.appendProperties(builder);
         builder.add(FACING);
+    }
+
+    @Override
+    public BlockState rotate(BlockState state, BlockRotation rotation) {
+        return state.with(FACING, rotation.rotate(state.get(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState state, BlockMirror mirror) {
+        return state.rotate(mirror.getRotation(state.get(FACING)));
     }
 }
