eacebbdc4143545ac3339f9fc5c5c7dfa34c079d
SirColor <macaraballo12@gmail.com>
2024-10-08 15:27:11 -0400

    Add ModItems

--- src/main/java/net/grapes/hexalia/item/ModItems.java
+++ src/main/java/net/grapes/hexalia/item/ModItems.java
@@ -2,9 +2,15 @@ package net.grapes.hexalia.item;
 
 import net.grapes.hexalia.HexaliaMod;
 import net.grapes.hexalia.block.ModBlocks;
+import net.grapes.hexalia.item.custom.KelpweaveBlade;
+import net.grapes.hexalia.item.custom.MandrakeItem;
+import net.grapes.hexalia.item.custom.PurifyingSaltsItem;
+import net.grapes.hexalia.item.custom.StoneDaggerItem;
+import net.grapes.hexalia.util.ModToolTiers;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.PlaceOnWaterBlockItem;
+import net.minecraft.world.item.Rarity;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -20,7 +26,7 @@ public class ModItems {
     public static final RegistryObject<Item> RESIN = ITEMS.register("resin",
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> ANCIENT_SEED = ITEMS.register("ancient_seed",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().rarity(Rarity.RARE)));
     public static final RegistryObject<Item> SILK_FIBER = ITEMS.register("silk_fiber",
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> SILKWORM = ITEMS.register("silkworm",
@@ -28,15 +34,16 @@ public class ModItems {
 
     // Crops, Plants & Seeds
     public static final RegistryObject<Item> SIREN_KELP = ITEMS.register("siren_kelp",
-            () -> new BlockItem(ModBlocks.SIREN_KELP.get(), new Item.Properties()));
+            () -> new BlockItem(ModBlocks.SIREN_KELP.get(), new Item.Properties()
+                    .food(ModFoodProperties.SIREN_KELP)));
     public static final RegistryObject<Item> MANDRAKE = ITEMS.register("mandrake",
-            () -> new Item(new Item.Properties()));
+            () -> new MandrakeItem(new Item.Properties()));
     public static final RegistryObject<Item> MANDRAKE_SEEDS = ITEMS.register("mandrake_seeds",
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> CHILLBERRIES = ITEMS.register("chillberries",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().food(ModFoodProperties.CHILLBERRIES)));
     public static final RegistryObject<Item> SUNFIRE_TOMATO = ITEMS.register("sunfire_tomato",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().food(ModFoodProperties.SUNFIRE_TOMATO)));
     public static final RegistryObject<Item> SUNFIRE_TOMATO_SEEDS = ITEMS.register("sunfire_tomato_seeds",
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> RABBAGE_SEEDS = ITEMS.register("rabbage_seeds",
@@ -44,7 +51,7 @@ public class ModItems {
     public static final RegistryObject<Item> LOTUS_FLOWER = ITEMS.register("lotus_flower",
             () -> new PlaceOnWaterBlockItem(ModBlocks.LOTUS_FLOWER.get(), new Item.Properties()));
     public static final RegistryObject<Item> SALTSPROUT = ITEMS.register("saltsprout",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().food(ModFoodProperties.SALTSPROUT)));
     public static final RegistryObject<Item> DUCKWEED = ITEMS.register("duckweed",
             () -> new PlaceOnWaterBlockItem(ModBlocks.DUCKWEED.get(), new Item.Properties()));
 
@@ -58,13 +65,13 @@ public class ModItems {
     public static final RegistryObject<Item> GHOST_FERN_POWDER = ITEMS.register("ghost_fern_powder",
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> PURIFYING_SALTS = ITEMS.register("purifying_salts",
-            () -> new Item(new Item.Properties()));
+            () -> new PurifyingSaltsItem(new Item.Properties()));
     public static final RegistryObject<Item> SPICY_SANDWICH = ITEMS.register("spicy_sandwich",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().food(ModFoodProperties.SPICY_SANDWICH)));
     public static final RegistryObject<Item> CHILLBERRY_PIE = ITEMS.register("chillberry_pie",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().food(ModFoodProperties.CHILLBERRY_PIE)));
     public static final RegistryObject<Item> MANDRAKE_STEW = ITEMS.register("mandrake_stew",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().food(ModFoodProperties.MANDRAKE_STEW)));
 
     // Brews
     public static final RegistryObject<Item> RUSTIC_BOTTLE = ITEMS.register("rustic_bottle",
@@ -88,7 +95,7 @@ public class ModItems {
     public static final RegistryObject<Item> SALT_LAMP = ITEMS.register("salt_lamp",
             () -> new BlockItem(ModBlocks.SALT_LAMP.get(), new Item.Properties()));
     public static final RegistryObject<Item> STONE_DAGGER = ITEMS.register("stone_dagger",
-            () -> new Item(new Item.Properties()));
+            () -> new StoneDaggerItem(new Item.Properties().durability(16)));
     public static final RegistryObject<Item> RITUAL_TABLE = ITEMS.register("ritual_table",
             () -> new BlockItem(ModBlocks.RITUAL_TABLE.get(),new Item.Properties()));
     public static final RegistryObject<Item> CANDLE_SKULL = ITEMS.register("candle_skull",
@@ -104,7 +111,8 @@ public class ModItems {
 
     // Weapons & Armor
     public static final RegistryObject<Item> KELPWEAVE_BLADE = ITEMS.register("kelpweave_blade",
-            () -> new Item(new Item.Properties()));
+            () -> new KelpweaveBlade(ModToolTiers.ANCIENT, 3, -2f,
+                    new Item.Properties().rarity(Rarity.RARE)));
     public static final RegistryObject<Item> RABBAGE = ITEMS.register("rabbage",
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> GHOSTVEIL = ITEMS.register("ghostveil",
