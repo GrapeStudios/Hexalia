0d2e800f21bb8a15fd75d3cbafd374508010847d
SirColor <macaraballo12@gmail.com>
2024-10-15 21:57:34 -0400

    Update GhostParticle

--- src/main/java/net/grapes/hexalia/particle/custom/GhostParticle.java
+++ src/main/java/net/grapes/hexalia/particle/custom/GhostParticle.java
@@ -1,18 +1,22 @@
 package net.grapes.hexalia.particle.custom;
 
-import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.*;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.core.particles.SimpleParticleType;
 
 public class GhostParticle extends TextureSheetParticle {
-    protected GhostParticle(ClientLevel pLevel, double pX, double pY, double pZ, double pXSpeed, double pYSpeed, double pZSpeed, float pScaleMultiplier, SpriteSet pSprites) {
-        super(pLevel, pX, pY, pZ, pXSpeed, pYSpeed, pZSpeed);
-        this.xd = pXSpeed;
-        this.yd = pYSpeed;
-        this.zd = pZSpeed;
-        this.quadSize *= pScaleMultiplier;
-        this.lifetime = 8;
-        this.setSpriteFromAge(pSprites);
+    protected GhostParticle(ClientLevel world, double x, double y, double z, double velocityX, double velocityY, double velocityZ, SpriteSet spriteSet) {
+        super(world, x, y, z, velocityX, velocityY, velocityZ);
+        this.friction = 0.98F;
+        this.gravity = 0.2F;
+        this.setSpriteFromAge(spriteSet);
+        this.quadSize *= 0.5F;
+        this.lifetime = 10;
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
     }
 
     @Override
@@ -21,12 +25,15 @@ public class GhostParticle extends TextureSheetParticle {
     }
 
     @Override
-    public void tick() {
-        super.tick();
-        this.yd -= 0.1F;
-        if (this.age++ >= this.lifetime) {
-            this.remove();
-        }
+    public void move(double dx, double dy, double dz) {
+        this.setBoundingBox(this.getBoundingBox().move(dx, dy, dz));
+        this.setLocationFromBoundingbox();
+    }
+
+    @Override
+    public float getQuadSize(float scaleFactor) {
+        float ageScale = ((float) this.age + scaleFactor) / (float) this.lifetime;
+        return this.quadSize * (1.0F - ageScale * ageScale * 0.5F);  // Decrease size over time
     }
 
     public static class Provider implements ParticleProvider<SimpleParticleType> {
@@ -37,9 +44,8 @@ public class GhostParticle extends TextureSheetParticle {
         }
 
         @Override
-        public Particle createParticle(SimpleParticleType type, ClientLevel world, double x, double y, double z, double vx, double vy, double vz) {
-            return new GhostParticle(world, x, y, z, vx, vy, vz, 0.5f, this.spriteSet);
+        public Particle createParticle(SimpleParticleType type, ClientLevel world, double x, double y, double z, double velocityX, double velocityY, double velocityZ) {
+            return new GhostParticle(world, x, y, z, velocityX, velocityY, velocityZ, this.spriteSet);
         }
     }
 }
-
