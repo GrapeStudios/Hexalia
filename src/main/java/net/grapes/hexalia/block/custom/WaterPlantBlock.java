a0a0407b4a0231f3897cd1c049a2df0ad92b9189
Bersanito <loewickes@gmail.com>
2024-11-10 16:49:42 +0100

    Remove unused imports

--- src/main/java/net/grapes/hexalia/block/custom/WaterPlantBlock.java
+++ src/main/java/net/grapes/hexalia/block/custom/WaterPlantBlock.java
@@ -2,17 +2,13 @@ package net.grapes.hexalia.block.custom;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
-import net.minecraft.world.level.block.IceBlock;
 import net.minecraft.world.level.block.WaterlilyBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
-import net.minecraft.world.level.material.FluidState;
-import net.minecraft.world.level.material.Fluids;
 import org.jetbrains.annotations.Nullable;
 
 public class WaterPlantBlock extends WaterlilyBlock {
