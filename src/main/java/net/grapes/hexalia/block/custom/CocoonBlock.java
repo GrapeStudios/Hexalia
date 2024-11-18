2f471d81e77199382362fae6b9fcf1dc68da427e
Bersanito <loewickes@gmail.com>
2024-11-18 20:32:40 +0100

    Clean up

--- src/main/java/net/grapes/hexalia/block/custom/CocoonBlock.java
+++ src/main/java/net/grapes/hexalia/block/custom/CocoonBlock.java
@@ -32,7 +32,6 @@ public class CocoonBlock extends Block {
 
     public static final DirectionProperty FACING = DirectionProperty.of("facing", Direction.Type.HORIZONTAL);
     public static final IntProperty HATCH = IntProperty.of("hatch", 0, 2);
-    private static final int HATCH_DELAY_TICKS = 60;
 
     private static final VoxelShape NORTH_SHAPE = VoxelShapes.cuboid(0.3125, 0.3125, 0.6875, 0.6875, 0.75, 1);
     private static final VoxelShape SOUTH_SHAPE = VoxelShapes.cuboid(0.3125, 0.3125, 0, 0.6875, 0.75, 0.3125);
