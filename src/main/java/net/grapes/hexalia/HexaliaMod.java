8b5683a1df7ee58382928a564ab2ca9be6bd7c17
Bersanito <loewickes@gmail.com>
2024-12-09 22:55:29 +0100

    Fixed Crash on sheets with HexaliaMod for Servers

--- src/main/java/net/grapes/hexalia/HexaliaMod.java
+++ src/main/java/net/grapes/hexalia/HexaliaMod.java
@@ -71,8 +71,7 @@ public class HexaliaMod
         event.enqueueWork(() -> {
             ModRegistries.registerCompostable();
 
-            Sheets.addWoodType(ModWoodTypes.COTTONWOOD);
-            Sheets.addWoodType(ModWoodTypes.WILLOW);
+
             ((FlowerPotBlock) Blocks.FLOWER_POT).addPlant(ModBlocks.SPIRIT_BLOOM.getId(), ModBlocks.POTTED_SPIRIT_BLOOM);
             ((FlowerPotBlock) Blocks.FLOWER_POT).addPlant(ModBlocks.DREAMSHROOM.getId(), ModBlocks.POTTED_DREAMSHROOM);
             ((FlowerPotBlock) Blocks.FLOWER_POT).addPlant(ModBlocks.HENBANE.getId(), ModBlocks.POTTED_HENBANE);
@@ -113,6 +112,8 @@ public class HexaliaMod
 
             EntityRenderers.register(ModEntities.SILK_MOTH_ENTITY.get(), SilkMothRenderer::new);
 
+            Sheets.addWoodType(ModWoodTypes.COTTONWOOD);
+            Sheets.addWoodType(ModWoodTypes.WILLOW);
         }
     }
 }
