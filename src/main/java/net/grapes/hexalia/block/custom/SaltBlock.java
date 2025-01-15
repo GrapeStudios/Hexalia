55181cb1b1b5e514e3e789da59184651ab8d8929
SirColor <macaraballo12@gmail.com>
2025-01-15 19:25:06 -0400

    Update SaltBlock

--- src/main/java/net/grapes/hexalia/block/custom/SaltBlock.java
+++ src/main/java/net/grapes/hexalia/block/custom/SaltBlock.java
@@ -1,35 +1,27 @@
 package net.grapes.hexalia.block.custom;
 
 import net.grapes.hexalia.block.entity.SaltBlockEntity;
+import net.grapes.hexalia.item.ModItems;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.Containers;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.storage.loot.LootParams;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.List;
 
 public class SaltBlock extends BaseEntityBlock {
 
@@ -45,87 +37,57 @@ public class SaltBlock extends BaseEntityBlock {
     }
 
     @Override
-    public boolean canSurvive(BlockState pState, LevelReader pLevel, BlockPos pPos) {
-        BlockPos blockPos = pPos.below();
-        BlockState blockState = pLevel.getBlockState(blockPos);
-        return this.canRunOnTop(pLevel, blockPos, blockState);
-    }
-
-    private boolean canRunOnTop(BlockGetter world, BlockPos pos, BlockState floor) {
-        return floor.isFaceSturdy(world, pos, Direction.UP) || floor.getBlock() == Blocks.HOPPER;
-    }
-
-    @Override
     public RenderShape getRenderShape(BlockState pState) {
         return RenderShape.MODEL;
     }
 
     @Override
-    public @NotNull List<ItemStack> getDrops(@NotNull BlockState blockState, LootParams.Builder builder) {
-        List<ItemStack> drops = super.getDrops(blockState, builder);
-        BlockEntity blockE = builder.getOptionalParameter(LootContextParams.BLOCK_ENTITY);
-        if (blockE instanceof SaltBlockEntity saltBlockEntity) {
-            drops.add(saltBlockEntity.getItem(0));
+    public void onRemove(BlockState pState, Level pLevel, BlockPos pPos, BlockState pNewState, boolean pIsMoving) {
+        if (pState.getBlock() != pNewState.getBlock()) {
+            BlockEntity blockEntity = pLevel.getBlockEntity(pPos);
+            if (blockEntity instanceof SaltBlockEntity saltBlockEntity) {
+                Containers.dropContents(pLevel, pPos, saltBlockEntity);
+            }
         }
-        return drops;
+        super.onRemove(pState, pLevel, pPos, pNewState, pIsMoving);
     }
 
     @Override
-    public InteractionResult use(BlockState state, Level world, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
-        InteractionResult result = InteractionResult.PASS;
+    public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
+        BlockEntity blockEntity = pLevel.getBlockEntity(pPos);
+        ItemStack heldItem = pPlayer.getItemInHand(pHand);
 
-        BlockEntity blockEntity = world.getBlockEntity(pos);
         if (!(blockEntity instanceof SaltBlockEntity saltBlockEntity)) {
-            return result;
-        }
-
-        if (saltBlockEntity.isEmpty()) {
-            result = addItemFromHand(world, saltBlockEntity, player, hand);
-        } else if (hand.equals(InteractionHand.MAIN_HAND)) {
-            removeItemFromBlock(world, saltBlockEntity, player);
-            result = InteractionResult.SUCCESS;
-        }
-
-        return result;
-    }
-
-
-    private InteractionResult addItemFromHand(Level world, SaltBlockEntity saltBlockEntity, Player player, InteractionHand hand) {
-        ItemStack heldItem = player.getItemInHand(hand);
-        ItemStack offHandItem = player.getOffhandItem();
-
-        if (!offHandItem.isEmpty()) {
-            if (hand.equals(InteractionHand.MAIN_HAND) && !(heldItem.getItem() instanceof BlockItem)) {
-                return InteractionResult.PASS;
-            }
-            if (hand.equals(InteractionHand.OFF_HAND)) {
-                return InteractionResult.PASS;
-            }
-        }
-        if (heldItem.isEmpty()) {
             return InteractionResult.PASS;
-        } else if (saltBlockEntity.addStack(player.getAbilities().instabuild ? heldItem.copy() : heldItem)) {
-            playItemSound(world, saltBlockEntity.getBlockPos());
-            return InteractionResult.SUCCESS;
+        }
+
+        if (pHand == InteractionHand.MAIN_HAND) {
+            if (!heldItem.isEmpty() && saltBlockEntity.isEmpty() && !heldItem.is(ModItems.HEX_FOCUS.get())) {
+                if (saltBlockEntity.addStack(heldItem.split(1))) {
+                    playItemSound(pLevel, pPos);
+                    return InteractionResult.sidedSuccess(pLevel.isClientSide);
+                }
+            }
+
+            if (!saltBlockEntity.isEmpty() && heldItem.isEmpty()) {
+                removeItemFromBlock(pLevel, saltBlockEntity, pPlayer);
+                return InteractionResult.sidedSuccess(pLevel.isClientSide);
+            }
         }
         return InteractionResult.PASS;
     }
 
-    private void removeItemFromBlock(Level world, SaltBlockEntity saltBlockEntity, Player player) {
-        BlockPos pos = saltBlockEntity.getBlockPos();
-        if (player.getAbilities().instabuild) {
-            saltBlockEntity.removeStack();
-        } else if (!player.getInventory().add(saltBlockEntity.removeStack())) {
-            Containers.dropItemStack(world, pos.getX(), pos.getY(), pos.getZ(), saltBlockEntity.removeStack());
+    private void removeItemFromBlock(Level pLevel, SaltBlockEntity saltBlockEntity, Player player) {
+        ItemStack stack = saltBlockEntity.removeStack();
+        if (!player.getInventory().add(stack)) {
+            Containers.dropItemStack(pLevel, player.getX(), player.getY(), player.getZ(), stack);
         }
-        playItemSound(world, pos);
+        playItemSound(pLevel, saltBlockEntity.getBlockPos());
     }
-
 
     private void playItemSound(Level pLevel, BlockPos pos) {
-        pLevel.playSound(null, pos, SoundEvents.ITEM_FRAME_ADD_ITEM, SoundSource.BLOCKS, 0.8f, 0.5f);
+        pLevel.playSound(null, pos, SoundEvents.ITEM_PICKUP, SoundSource.BLOCKS, 0.5f, 0.5f);
     }
-
 
     @Nullable
     @Override
