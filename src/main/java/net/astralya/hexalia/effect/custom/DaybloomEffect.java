9b9cc409b0c97ec22a69d9b71531e4abf39cdb3b
SirColor <macaraballo12@gmail.com>
2025-10-02 17:26:18 -0400

    Add SunlightCheck

--- src/main/java/net/astralya/hexalia/effect/custom/DaybloomEffect.java
+++ src/main/java/net/astralya/hexalia/effect/custom/DaybloomEffect.java
@@ -1,20 +1,19 @@
 package net.astralya.hexalia.effect.custom;
 
-import net.minecraft.entity.attribute.EntityAttributeInstance;
+import net.astralya.hexalia.util.SunlightCheck;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
 
 import java.util.UUID;
 
 public class DaybloomEffect extends StatusEffect {
-
     private static final int COOLDOWN = 100;
-    private static final UUID SPEED_MODIFIER_UUID = UUID.fromString("e1234567-89ab-cdef-0123-456789abcdef");
+    private static final UUID SPEED_MODIFIER_ID = UUID.fromString("6b9a6c0c-1f2e-4b7d-9f8c-9a3e1c9d2d5e");
 
     public DaybloomEffect(StatusEffectCategory category, int color) {
         super(category, color);
@@ -22,48 +21,36 @@ public class DaybloomEffect extends StatusEffect {
 
     @Override
     public void applyUpdateEffect(LivingEntity entity, int amplifier) {
-        if (entity instanceof PlayerEntity player) {
-            World world = player.getWorld();
-
-            if (isNight(world)) {
-                player.damage(player.getDamageSources().magic(), 1.5F);
-                removeSpeedModifier(player);
-            } else if (isDay(world)) {
-                player.heal(2.0F);
-                applySpeedModifier(player, amplifier);
-            } else {
-                removeSpeedModifier(player);
-            }
+        if (!(entity instanceof PlayerEntity player)) return;
+        World world = player.getWorld();
+        SunlightCheck sc = new SunlightCheck(world, player.getBlockPos());
+        sc.recheckCanSeeSun();
+        float gen = sc.getGenerationMultiplier();
+        if (gen <= 0.0F) {
+            player.damage(player.getDamageSources().magic(), 1.5F);
+            removeSpeedModifier(player);
+        } else {
+            player.heal(2.0F);
+            applySpeedModifier(player, amplifier);
         }
-    }
-
-    private boolean isDay(World world) {
-        long time = world.getTimeOfDay() % 24000;
-        return time >= 0 && time < 13000;
-    }
-
-    private boolean isNight(World world) {
-        long time = world.getTimeOfDay() % 24000;
-        return time >= 13000 && time <= 23000;
     }
 
     private void applySpeedModifier(PlayerEntity player, int amplifier) {
-        EntityAttributeInstance speedAttribute = player.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED);
-        if (speedAttribute != null) {
-            removeSpeedModifier(player);
-
-            double speedBoost = 0.05 * (amplifier + 1);
-            speedAttribute.addTemporaryModifier(
-                    new EntityAttributeModifier(SPEED_MODIFIER_UUID, "Daybloom Speed Boost", speedBoost, EntityAttributeModifier.Operation.ADDITION)
-            );
-        }
+        var inst = player.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED);
+        if (inst == null) return;
+        inst.removeModifier(SPEED_MODIFIER_ID);
+        double speedBoost = 0.05 * (amplifier + 1);
+        inst.addTemporaryModifier(new EntityAttributeModifier(
+                SPEED_MODIFIER_ID,
+                "daybloom_speed",
+                speedBoost,
+                EntityAttributeModifier.Operation.MULTIPLY_TOTAL
+        ));
     }
 
     private void removeSpeedModifier(PlayerEntity player) {
-        EntityAttributeInstance speedAttribute = player.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED);
-        if (speedAttribute != null) {
-            speedAttribute.removeModifier(SPEED_MODIFIER_UUID);
-        }
+        var inst = player.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED);
+        if (inst != null) inst.removeModifier(SPEED_MODIFIER_ID);
     }
 
     @Override
