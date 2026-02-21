0fa35766f762791a5dc54b339b7151d737c9ee70
SirColor <macaraballo12@gmail.com>
2026-02-21 19:22:57 -0400

    Update Spore Particle

--- src/main/java/net/astralya/hexalia/particle/custom/SporeParticle.java
+++ src/main/java/net/astralya/hexalia/particle/custom/SporeParticle.java
@@ -1,18 +1,34 @@
 package net.astralya.hexalia.particle.custom;
 
 import net.minecraft.client.multiplayer.ClientLevel;
-import net.minecraft.client.particle.*;
+import net.minecraft.client.particle.Particle;
+import net.minecraft.client.particle.ParticleProvider;
+import net.minecraft.client.particle.ParticleRenderType;
+import net.minecraft.client.particle.SpriteSet;
+import net.minecraft.client.particle.TextureSheetParticle;
 import net.minecraft.core.particles.SimpleParticleType;
+import net.minecraft.util.Mth;
 
 public class SporeParticle extends TextureSheetParticle {
+    private final SpriteSet sprites;
 
     public SporeParticle(ClientLevel level, double x, double y, double z, double velocityX, double velocityY, double velocityZ, SpriteSet spriteSet) {
         super(level, x, y, z, velocityX, velocityY, velocityZ);
+        this.sprites = spriteSet;
+
         this.xd = velocityX;
         this.yd = velocityY;
         this.zd = velocityZ;
+
         this.quadSize *= 0.1f;
         this.lifetime = 40 + this.random.nextInt(12);
+
+        this.gravity = 0.01f;
+        this.friction = 0.92f;
+
+        this.hasPhysics = true;
+
+        this.alpha = 0.0f;
         this.pickSprite(spriteSet);
     }
 
@@ -22,9 +38,36 @@ public class SporeParticle extends TextureSheetParticle {
     }
 
     @Override
-    public void move(double x, double y, double z) {
-        this.setBoundingBox(this.getBoundingBox().move(x, y, z));
-        this.setPos(this.x + x, this.y + y, this.z + z);
+    public void tick() {
+        this.xo = this.x;
+        this.yo = this.y;
+        this.zo = this.z;
+
+        if (this.age++ >= this.lifetime) {
+            this.remove();
+            return;
+        }
+
+        this.pickSprite(this.sprites);
+
+        float lifeT = (float) this.age / (float) this.lifetime;
+        this.alpha = Mth.clamp(lifeT * 6.0f, 0.0f, 1.0f);
+        if (lifeT > 0.75f) {
+            this.alpha *= Mth.clamp((1.0f - lifeT) / 0.25f, 0.0f, 1.0f);
+        }
+
+        this.xd += (this.random.nextDouble() - 0.5) * 0.0025;
+        this.zd += (this.random.nextDouble() - 0.5) * 0.0025;
+
+        this.yd -= this.gravity;
+
+        this.move(this.xd, this.yd, this.zd);
+
+        if (this.onGround) {
+            this.xd *= 0.65;
+            this.zd *= 0.65;
+            this.yd *= -0.15;
+        }
     }
 
     public static class Factory implements ParticleProvider<SimpleParticleType> {
